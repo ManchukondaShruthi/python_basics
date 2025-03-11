ff8f4e46029513dbc1776b4a7f0ad4e30afc2cc5
Shruthi Manchukonda <shruthimanchukonda@Shruthis-MacBook-Pro.local>
2025-03-11 10:52:46 -0700

    python-reverse-a-number-without-string-method

--- reversenumber.py
+++ reversenumber.py
@@ -2,6 +2,15 @@
 #input = 125, output = 521
 
 def reversenumber(n):
-    x = ""
-    x.reverse(n)
-reversenumber(125)
+    count = 0
+    digit_list = []
+    while(n > 0):
+        remainder = n % 10
+        quotient = n // 10
+        digit_list.append(remainder)
+        n = quotient
+        count = count + 1
+
+    print(digit_list)
+    return count
+print(reversenumber(125))
